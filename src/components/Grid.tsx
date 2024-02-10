9eb245bac142d44c0780fbcb0a252bbba7f3f344
dlstenbro <danielstenbro101@hotmail.com>
2024-02-10 00:56:55 -0600

    add state rendering for api call; display cards in card

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,17 +1,32 @@
 import * as React from 'react'
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
+import { Pokemon } from '../services';
+import { PokemonCard } from './Card';
+import { Container } from '@mui/material';
 
-function PokemonGrid(props: {
-    rowSpacing: number,
-    colSpacing: number
-}, data) {
+function PokemonGrid(props:{
+    spacing : number, 
+    data : Array<Pokemon>
+}) {
     return (
-        <Grid 
-            rowSpacing={props.rowSpacing} 
-            columnSpacing={props.colSpacing}>
-                {data}
-        </Grid>
+        <Container sx={{ flexGrow: 1 }}>
+            <Grid container spacing={props.spacing} rowSpacing={20}>
+                {props.data.map(e => (
+                        <Grid padding={2}>
+                            <PokemonCard 
+                                key={e.id} 
+                                headerData={e.name} 
+                                contentData={e.name}
+                                imgUrl={e.imgUrl} 
+                                imgTitle={e.name}
+                                url={e.url}
+                                />
+                        </Grid>
+                    ))}
+            </Grid>
+      </Container>
     )
 }
     
